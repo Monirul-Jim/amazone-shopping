c51a4f68503cdcd3ad26ff4b8d308adc56b70831
unknown <Monirul-Jim>
2023-04-06 22:23:26 +0600

    path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLoaderData,
+} from "react-router-dom";
+import Home from './layout/Home';
+import Shop from './component/shop/Shop';
+import Orders from './component/orders/Orders';
+import Inventory from './component/inventory/Inventory';
+import Login from './component/login/Login';
+const router=createBrowserRouter([
+   {
+    path:'/',
+    element:<Home></Home>,
+    children:[
+      {
+        path:'/',
+        element:<Shop></Shop>
+      },
+      {
+        path:"/orders",
+        element:<Orders></Orders>
+      },
+      {
+        path:'/inventory',
+        element:<Inventory></Inventory>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      }
+    ]
+   }
+    
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} ></RouterProvider>
   </React.StrictMode>,
 )
